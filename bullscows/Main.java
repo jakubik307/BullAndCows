429f0ccab9b6b75d47850c73f17976dbf9edbc2e
jakubik307 <and.jj.an@gmail.com>
2022-12-06 12:21:10 +0100

    Player can choose code length and play whole game

--- bullscows/Main.java
+++ bullscows/Main.java
@@ -5,71 +5,71 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
-    static int[] secretCode = {2, 1, 3, 7};
+    static StringBuilder secretCode = new StringBuilder();
+    static int turn = 1;
 
-    public static int[] getPlayerGuess() {
+    public static StringBuilder getPlayerGuess() {
+        System.out.println("Turn " + turn + ":");
+        turn++;
+
         Scanner scanner = new Scanner(System.in);
-        int input;
-        do {
-            input = scanner.nextInt();
-        } while (input <= 999 || input >= 10000);
+        StringBuilder guess = new StringBuilder();
 
-        int[] guess = new int[4];
-        guess[0] = input / 1000;
-        input -= (input / 1000) * 1000;
-        guess[1] = input / 100;
-        input -= (input / 100) * 100;
-        guess[2] = input / 10;
-        input -= (input / 10) * 10;
-        guess[3] = input;
-
+        guess.append(scanner.next());
         return guess;
     }
 
-    public static void gradePlayerGuess(int[] guess) {
+    public static boolean gradePlayerGuess(StringBuilder guess) {
         int bulls = 0;
         int cows = 0;
 
-        for (int i = 0; i < secretCode.length; i++) {
-            for (int j = 0; j < guess.length; j++) {
-                if (i == j && secretCode[i] == guess[j]) {
+        for (int i = 0; i < secretCode.length(); i++) {
+            for (int j = 0; j < guess.length(); j++) {
+                if (i == j && secretCode.charAt(i) == guess.charAt(j)) {
                     bulls++;
-                } else if (secretCode[i] == guess[j]) {
+                } else if (secretCode.charAt(i) == guess.charAt(j)) {
                     cows++;
                 }
             }
         }
 
-        if (cows != 0 && bulls != 0) {
-            System.out.println("Grade: " + bulls + " bull(s) and " + cows + " cow(s). The secret code is " + secretCode[0] + secretCode[1] + secretCode[2] + secretCode[3]);
+        if (bulls == secretCode.length()) {
+            System.out.println("Grade: " + bulls + " bull(s).");
+            System.out.println("Congratulations! You guessed the secret code.");
+            return false;
+        } else if (cows != 0 && bulls != 0) {
+            System.out.println("Grade: " + bulls + " bull(s) and " + cows + " cow(s).");
+            return true;
         } else if (cows != 0) {
-            System.out.println("Grade: " + cows + " cow(s). The secret code is " + secretCode[0] + secretCode[1] + secretCode[2] + secretCode[3]);
+            System.out.println("Grade: " + cows + " cow(s).");
+            return true;
         } else if (bulls != 0) {
-            System.out.println("Grade: " + bulls + " bull(s). The secret code is " + secretCode[0] + secretCode[1] + secretCode[2] + secretCode[3]);
+            System.out.println("Grade: " + bulls + " bull(s).");
+            return true;
         } else {
-            System.out.println("Grade: None. The secret code is " + secretCode[0] + secretCode[1] + secretCode[2] + secretCode[3]);
+            System.out.println("Grade: None.");
+            return true;
         }
     }
 
     public static void generateSecretCode() {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
-        StringBuilder code = new StringBuilder();
 
-        if (size > 0 && size < 11) {
-            ArrayList<Integer> list = new ArrayList<Integer>();
+        if (size > 0 && size < 10) {
+            ArrayList<Integer> list = new ArrayList<>();
             for (int i = 1; i < 10; i++) list.add(i);
             Collections.shuffle(list);
-            for (int i = 0; i < size; i++) code.append(list.get(i));
-            System.out.printf("The random secret number is %s.", code);
+            for (int i = 0; i < size; i++) secretCode.append(list.get(i));
+            System.out.println("Okay, let's start a game!");
         } else {
-            System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
+            System.out.println("Error: can't generate a secret number with a length of" + size + "because there aren't enough unique digits.");
         }
 
     }
 
     public static void main(String[] args) {
-        //gradePlayerGuess(getPlayerGuess());
         generateSecretCode();
+        while (gradePlayerGuess(getPlayerGuess())) ;
     }
 }
