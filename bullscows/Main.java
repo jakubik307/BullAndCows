662e6b0c41f4af172b3e4e1c81fd6bf8f321193a
jakubik307 <and.jj.an@gmail.com>
2022-12-06 19:47:42 +0100

    Add option of choosing number of all possible characters

--- bullscows/Main.java
+++ bullscows/Main.java
@@ -7,6 +7,9 @@ import java.util.Scanner;
 public class Main {
     static StringBuilder secretCode = new StringBuilder();
     static int turn = 1;
+    static int numOfPossibleSymbols;
+
+    static StringBuilder allSymbols = new StringBuilder("0123456789abcdefghijklmnopqrstuvwxyz");
 
     public static StringBuilder getPlayerGuess() {
         System.out.println("Turn " + turn + ":");
@@ -17,6 +20,38 @@ public class Main {
 
         guess.append(scanner.next());
         return guess;
+    }
+
+    public static void generateSecretCode() {
+        System.out.println("Input the lenght of the secret code:");
+
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+
+        System.out.println("Input the number of possible symbols in the code:");
+        numOfPossibleSymbols = scanner.nextInt();
+
+        if (size > 0 && size <= numOfPossibleSymbols) {
+            ArrayList<Character> list = new ArrayList<>();
+            for (int i = 1; i < numOfPossibleSymbols; i++) list.add(allSymbols.charAt(i));
+            Collections.shuffle(list);
+            for (int i = 0; i < size; i++) secretCode.append(list.get(i));
+            System.out.print("The secret is prepared: ");
+            for (int i = 0; i < size; i++) {
+                System.out.print("*");
+            }
+            System.out.print("\n(0-");
+            if (numOfPossibleSymbols <= 10) {
+                System.out.print(allSymbols.charAt(numOfPossibleSymbols - 1) + ").\n");
+            } else if (numOfPossibleSymbols == 11) {
+                System.out.print("9, a).\n");
+            } else {
+                System.out.print("9, a-" + allSymbols.charAt(numOfPossibleSymbols - 1) + ").\n");
+            }
+            System.out.println("Okay, let's start a game!");
+        } else {
+            System.out.println("Error: can't generate a secret number with a length of " + size + " because there aren't enough unique digits.");
+        }
     }
 
     public static boolean gradePlayerGuess(StringBuilder guess) {
@@ -50,22 +85,6 @@ public class Main {
             System.out.println("Grade: None.");
             return true;
         }
-    }
-
-    public static void generateSecretCode() {
-        Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
-
-        if (size > 0 && size < 10) {
-            ArrayList<Integer> list = new ArrayList<>();
-            for (int i = 1; i < 10; i++) list.add(i);
-            Collections.shuffle(list);
-            for (int i = 0; i < size; i++) secretCode.append(list.get(i));
-            System.out.println("Okay, let's start a game!");
-        } else {
-            System.out.println("Error: can't generate a secret number with a length of" + size + "because there aren't enough unique digits.");
-        }
-
     }
 
     public static void main(String[] args) {
